e53b2b8dab41f1e08543aa6ec20a9cbfe6c4ad5e
Success Agboyinu <42468250+SuccessAT@users.noreply.github.com>
2022-11-01 11:06:21 +0100

    Update urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('create', CourseCreateView.as_view(), name='course_create'),
     #path('<int:pk>/edit', CourseDetailDeleteView.as_view(), name='course_edit'),
     path('lesson/<int:pk>', LessonView.as_view(), name = 'lesson'),
-    path('video/<int:pk>', VideoView.as_view(), name = 'video'),
+    #path('video/<int:pk>', VideoView.as_view(), name = 'video'),
     path('aboutus', AboutUsView.as_view(), name='aboutus')
     #path('enrollment', EnrollmentOnCourseView.as_view(), name='enrollment'),
 
