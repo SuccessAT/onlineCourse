ca007deba90156e65a35e0d1e530d3886f19875d
Success Agboyinu <42468250+SuccessAT@users.noreply.github.com>
2022-11-01 04:31:12 +0100

    Update admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Course, Lesson, Video
 from embed_video.admin import AdminVideoMixin
 
+class videoAdmin(AdminVideoMixin, admin.ModelAdmin):
+    pass
+  
 # Register your models here.
 admin.site.register([Course, Lesson, Video])
+admin.site.register(Video, videoAdmin)
